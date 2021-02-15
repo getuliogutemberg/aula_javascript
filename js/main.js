83bc87ec99e71876bf2420a3b0b03942a58d092e
GetulioGutemberg <getulio.dev@gmail.com>
2021-02-15 16:51:36 -0300

    curso de js do Dio(Finalizado)

---
+++ js/main.js
@@ -0,0 +1,137 @@
+//var nome = "Getulio Gutemberg";
+//var idade = 31;
+//var idade2 = 10;
+//var frase = "Brasil é uma Aventura";
+//var n1 = 5;
+//var n2 = 3;
+//alert("Bem vindo " + nome + ". Este é meu primeiro js!");
+//alert(idade+idade2);
+//console.log(nome)
+//console.log(idade)
+//console.log(frase.replace("Brasil","Japão"));
+//alert(frase.replace("Brasil","Japão")); 
+//console.log(frase.toLocaleLowerCase());
+//console.log(frase.toUpperCase());
+//console.log(n1 * n2);
+//alert(n1 / n2);
+//var lista = ["maça","pera","laranja"];
+/*console.log(lista);
+console.log(lista[1]);
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.toString()[0]);
+console.log(lista.join(" - "));*/
+/*var frutas = {nome:"maça", cor:"vermelha"};
+console.log(frutas);
+console.log(frutas.nome);
+console.log(frutas.cor);
+console.log(frutas.nome,frutas.cor);
+console.log(frutas[0])*/
+/*var frutas = [{nome:"maça", cor:"vermelha"},{nome:"uva", cor:"roxo"}];
+console.log(frutas);
+console.log(frutas[1].nome);
+console.log(frutas[0].cor);
+console.log(frutas[0].nome,frutas[1].cor);
+console.log(frutas[0]);
+*/
+//var idade = 18;
+//var idade = prompt("Qual sua idade?")
+//if (idade >= 18){
+//    alert("Maior de idade");
+//}else{
+//    alert("Menor de idade");
+
+//}
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    count = count + 1 ;
+//  counte++;
+}
+
+var count2;
+
+for(count2=0; count2 <= 5 ; count2++ ){
+    alert(count2);
+}
+
+*/
+/*
+
+var d = new Date();
+//alert(d);
+//alert(d.getMonth());
+alert("hoje é " + d.getDate() + "/" d.getMounth()+1);*/
+/*
+function soma(n1,n2){
+    return n1 + n2;
+}
+
+function setReplace(frase,nome,novo_nome){
+    return frase.replace(nome,novo_nome);
+}
+
+alert(soma(5,10));
+alert(setReplace("vai Japao!","Japao","brasil"));
+
+
+var idade = prompt("Digite sua Idade");
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+    validar = true;
+    }else{
+    validar = false;
+    }
+    return validar;
+}
+alert(validaIdade(idade));
+console.log(validaIdade((idade)))
+
+*/
+/*
+
+function clicou(){
+    //alert("voce clicou");
+    //document.getElementById("agradecimento").innerHTML = "Obrigado por clicar modificado";
+    //console.log(document.getElementById("agradecimento"));
+    //console.log(document.getElementById("agradecimento"));
+    //document.getElementById("agradecimento").innerHTML = "<b>Obrigado por Clicar</b>";
+}*/
+/*
+function redirecionar(){
+    window.open("https://www.google.com.br");
+    //window.location.href = "https://www.google.com.br";
+}
+
+
+
+function trocar(elemento){
+    //alert("trocar texto");
+    //document.getElementById("mousemove").innerHTML = "obrigado por passar o mouse";
+    elemento.innerHTML = "Valeu"
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "PASSE O MOUSE AQUI";
+    elemento.innerHTML = "volta aqui"
+
+
+}
+
+function load(){
+    alert("Pagina Carregada!!!");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
+
+*/
